ff40edb6cc425afe82ca031d760286713710eea2
Uldéric KIBONGUI <ulderic.k@free.fr>
2017-02-15 01:46:06 +0100

    manual transpilation

--- index.js
+++ index.js
@@ -3,98 +3,98 @@
 
 'use strict';
 
-const data_decoders = {
+var data_decoders = {
   pcm8: function (buffer, offset, output, channels, samples) {
-    let input = new Uint8Array(buffer, offset);
-    let pos = 0;
-    for (let i = 0; i < samples; ++i) {
-      for (let ch = 0; ch < channels; ++ch) {
-        let data = input[pos++] - 128;
+    var input = new Uint8Array(buffer, offset);
+    var pos = 0;
+    for (var i = 0; i < samples; ++i) {
+      for (var ch = 0; ch < channels; ++ch) {
+        var data = input[pos++] - 128;
         output[ch][i] = data < 0 ? data / 128 : data / 127;
       }
     }
   },
   pcm16: function (buffer, offset, output, channels, samples) {
-    let input = new Int16Array(buffer, offset);
-    let pos = 0;
-    for (let i = 0; i < samples; ++i) {
-      for (let ch = 0; ch < channels; ++ch) {
-        let data = input[pos++];
+    var input = new Int16Array(buffer, offset);
+    var pos = 0;
+    for (var i = 0; i < samples; ++i) {
+      for (var ch = 0; ch < channels; ++ch) {
+        var data = input[pos++];
         output[ch][i] = data < 0 ? data / 32768 : data / 32767;
       }
     }
   },
   pcm24: function (buffer, offset, output, channels, samples) {
-    let input = new Uint8Array(buffer, offset);
-    let pos = 0;
-    for (let i = 0; i < samples; ++i) {
-      for (let ch = 0; ch < channels; ++ch) {
-        let x0 = input[pos++];
-        let x1 = input[pos++];
-        let x2 = input[pos++];
-        let xx = (x0 + (x1 << 8) + (x2 << 16));
-        let data = xx > 0x800000 ? xx - 0x1000000 : xx;
+    var input = new Uint8Array(buffer, offset);
+    var pos = 0;
+    for (var i = 0; i < samples; ++i) {
+      for (var ch = 0; ch < channels; ++ch) {
+        var x0 = input[pos++];
+        var x1 = input[pos++];
+        var x2 = input[pos++];
+        var xx = (x0 + (x1 << 8) + (x2 << 16));
+        var data = xx > 0x800000 ? xx - 0x1000000 : xx;
         output[ch][i] = data < 0 ? data / 8388608 : data / 8388607;
       }
     }
   },
   pcm32: function (buffer, offset, output, channels, samples) {
-    let input = new Int32Array(buffer, offset);
-    let pos = 0;
-    for (let i = 0; i < samples; ++i) {
-      for (let ch = 0; ch < channels; ++ch) {
-        let data = input[pos++];
+    var input = new Int32Array(buffer, offset);
+    var pos = 0;
+    for (var i = 0; i < samples; ++i) {
+      for (var ch = 0; ch < channels; ++ch) {
+        var data = input[pos++];
         output[ch][i] = data < 0 ? data / 2147483648 : data / 2147483647;
       }
     }
   },
   pcm32f: function (buffer, offset, output, channels, samples) {
-    let input = new Float32Array(buffer, offset);
-    let pos = 0;
-    for (let i = 0; i < samples; ++i) {
-      for (let ch = 0; ch < channels; ++ch)
+    var input = new Float32Array(buffer, offset);
+    var pos = 0;
+    for (var i = 0; i < samples; ++i) {
+      for (var ch = 0; ch < channels; ++ch)
         output[ch][i] = input[pos++];
     }
   },
   pcm64f: function (buffer, offset, output, channels, samples) {
-    let input = new Float64Array(buffer, offset);
-    let pos = 0;
-    for (let i = 0; i < samples; ++i) {
-      for (let ch = 0; ch < channels; ++ch)
+    var input = new Float64Array(buffer, offset);
+    var pos = 0;
+    for (var i = 0; i < samples; ++i) {
+      for (var ch = 0; ch < channels; ++ch)
         output[ch][i] = input[pos++];
     }
   },
 };
 
-const data_encoders = {
+var data_encoders = {
   pcm8: function (buffer, offset, input, channels, samples) {
-    let output = new Uint8Array(buffer, offset);
-    let pos = 0;
-    for (let i = 0; i < samples; ++i) {
-      for (let ch = 0; ch < channels; ++ch) {
-        let v = Math.max(-1, Math.min(input[ch][i], 1));
+    var output = new Uint8Array(buffer, offset);
+    var pos = 0;
+    for (var i = 0; i < samples; ++i) {
+      for (var ch = 0; ch < channels; ++ch) {
+        var v = Math.max(-1, Math.min(input[ch][i], 1));
         v = ((v * 0.5 + 0.5) * 255) | 0;
         output[pos++] = v;
       }
     }
   },
   pcm16: function (buffer, offset, input, channels, samples) {
-    let output = new Int16Array(buffer, offset);
-    let pos = 0;
-    for (let i = 0; i < samples; ++i) {
-      for (let ch = 0; ch < channels; ++ch) {
-        let v = Math.max(-1, Math.min(input[ch][i], 1));
+    var output = new Int16Array(buffer, offset);
+    var pos = 0;
+    for (var i = 0; i < samples; ++i) {
+      for (var ch = 0; ch < channels; ++ch) {
+        var v = Math.max(-1, Math.min(input[ch][i], 1));
         v = ((v < 0) ? v * 32768 : v * 32767) | 0;
         output[pos++] = v;
       }
     }
   },
   pcm24: function (buffer, offset, input, channels, samples) {
-    let output = new Uint8Array(buffer, offset);
-    let pos = 0;
-    for (let i = 0; i < samples; ++i) {
-      for (let ch = 0; ch < channels; ++ch) {
-        let v = Math.max(-1, Math.min(input[ch][i], 1));
+    var output = new Uint8Array(buffer, offset);
+    var pos = 0;
+    for (var i = 0; i < samples; ++i) {
+      for (var ch = 0; ch < channels; ++ch) {
+        var v = Math.max(-1, Math.min(input[ch][i], 1));
         v = ((v < 0) ? 0x1000000 + v * 8388608 : v * 8388607) | 0;
         output[pos++] = (v >> 0) & 0xff;
         output[pos++] = (v >> 8) & 0xff;
@@ -103,32 +103,32 @@ const data_encoders = {
     }
   },
   pcm32: function (buffer, offset, input, channels, samples) {
-    let output = new Int32Array(buffer, offset);
-    let pos = 0;
-    for (let i = 0; i < samples; ++i) {
-      for (let ch = 0; ch < channels; ++ch) {
-        let v = Math.max(-1, Math.min(input[ch][i], 1));
+    var output = new Int32Array(buffer, offset);
+    var pos = 0;
+    for (var i = 0; i < samples; ++i) {
+      for (var ch = 0; ch < channels; ++ch) {
+        var v = Math.max(-1, Math.min(input[ch][i], 1));
         v = ((v < 0) ? v * 2147483648 : v * 2147483647) | 0;
         output[pos++] = v;
       }
     }
   },
   pcm32f: function (buffer, offset, input, channels, samples) {
-    let output = new Float32Array(buffer, offset);
-    let pos = 0;
-    for (let i = 0; i < samples; ++i) {
-      for (let ch = 0; ch < channels; ++ch) {
-        let v = Math.max(-1, Math.min(input[ch][i], 1));        
+    var output = new Float32Array(buffer, offset);
+    var pos = 0;
+    for (var i = 0; i < samples; ++i) {
+      for (var ch = 0; ch < channels; ++ch) {
+        var v = Math.max(-1, Math.min(input[ch][i], 1));
         output[pos++] = v;
       }
     }
   },
   pcm64f: function (buffer, offset, input, channels, samples) {
-    let output = new Float64Array(buffer, offset);
-    let pos = 0;
-    for (let i = 0; i < samples; ++i) {
-      for (let ch = 0; ch < channels; ++ch) {
-        let v = Math.max(-1, Math.min(input[ch][i], 1));        
+    var output = new Float64Array(buffer, offset);
+    var pos = 0;
+    for (var i = 0; i < samples; ++i) {
+      for (var ch = 0; ch < channels; ++ch) {
+        var v = Math.max(-1, Math.min(input[ch][i], 1));
         output[pos++] = v;
       }
     }
@@ -136,15 +136,15 @@ const data_encoders = {
 };
 
 function lookup(table, bitDepth, floatingPoint) {
-  let name = 'pcm' + bitDepth + (floatingPoint ? 'f' : '');
-  let fn = table[name];
+  var name = 'pcm' + bitDepth + (floatingPoint ? 'f' : '');
+  var fn = table[name];
   if (!fn)
     throw new TypeError('Unsupported data format: ' + name);
   return fn;
 }
 
 function decode(buffer) {
-  let pos = 0, end = 0;
+  var pos = 0, end = 0;
   if (buffer.buffer) {
     // If we are handed a typed array or a buffer, then we have to consider the
     // offset and length into the underlying array buffer.
@@ -158,29 +158,29 @@ function decode(buffer) {
     end = buffer.byteLength;
   }
 
-  let v = new DataView(buffer);
+  var v = new DataView(buffer);
 
   function u8() {
-    let x = v.getUint8(pos);
+    var x = v.getUint8(pos);
     pos++;
     return x;
   }
 
   function u16() {
-    let x = v.getUint16(pos, true);
+    var x = v.getUint16(pos, true);
     pos += 2;
     return x;
   }
 
   function u32() {
-    let x = v.getUint32(pos, true);
+    var x = v.getUint32(pos, true);
     pos += 4;
     return x;
   }
 
   function string(len) {
-    let str = '';
-    for (let i = 0; i < len; ++i)
+    var str = '';
+    for (var i = 0; i < len; ++i)
       str += String.fromCharCode(u8());
     return str;
   }
@@ -191,15 +191,15 @@ function decode(buffer) {
   if (string(4) !== 'WAVE')
     throw new TypeError('Invalid WAV file');
 
-  let fmt;
-  
+  var fmt;
+
   while (pos < end) {
-    let type = string(4);
-    let size = u32();
-    let next = pos + size;
+    var type = string(4);
+    var size = u32();
+    var next = pos + size;
     switch (type) {
     case 'fmt ':
-      let formatId = u16();
+      var formatId = u16();
       if (formatId !== 0x0001 && formatId !== 0x0003)
         throw new TypeError('Unsupported format in WAV file: ' + formatId.toString(16));
       fmt = {
@@ -215,11 +215,11 @@ function decode(buffer) {
     case 'data':
       if (!fmt)
         throw new TypeError('Missing "fmt " chunk.');
-      let samples = Math.floor(size / fmt.blockSize);
-      let channels = fmt.channels;
-      let sampleRate = fmt.sampleRate;
-      let channelData = [];
-      for (let ch = 0; ch < channels; ++ch)
+      var samples = Math.floor(size / fmt.blockSize);
+      var channels = fmt.channels;
+      var sampleRate = fmt.sampleRate;
+      var channelData = [];
+      for (var ch = 0; ch < channels; ++ch)
         channelData[ch] = new Float32Array(samples);
       lookup(data_decoders, fmt.bitDepth, fmt.floatingPoint)(buffer, pos, channelData, channels, samples);
       return {
@@ -233,15 +233,15 @@ function decode(buffer) {
 }
 
 function encode(channelData, opts) {
-  let sampleRate = opts.sampleRate || 16000;
-  let floatingPoint = !!(opts.float || opts.floatingPoint);
-  let bitDepth = floatingPoint ? 32 : ((opts.bitDepth | 0) || 16);
-  let channels = channelData.length;
-  let samples = channelData[0].length;
-  let buffer = new ArrayBuffer(44 + (samples * channels * (bitDepth >> 3)));
+  var sampleRate = opts.sampleRate || 16000;
+  var floatingPoint = !!(opts.float || opts.floatingPoint);
+  var bitDepth = floatingPoint ? 32 : ((opts.bitDepth | 0) || 16);
+  var channels = channelData.length;
+  var samples = channelData[0].length;
+  var buffer = new ArrayBuffer(44 + (samples * channels * (bitDepth >> 3)));
 
-  let v = new DataView(buffer);
-  let pos = 0;
+  var v = new DataView(buffer);
+  var pos = 0;
 
   function u8(x) {
     v.setUint8(pos++, x);
@@ -256,7 +256,7 @@ function encode(channelData, opts) {
     v.setUint32(pos, x, true);
     pos += 4;
   }
-  
+
   function string(s) {
     for (var i = 0; i < s.length; ++i)
       u8(s.charCodeAt(i));
